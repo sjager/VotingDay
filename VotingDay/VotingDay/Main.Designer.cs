9353e47a11d024ca8e003d7a06e118b0e1c53bf0
Sawyer Jager <sawyer.jgr@gmail.com>
2015-02-21 23:37:54 -0600

    Create initial project structure.

---
+++ VotingDay/VotingDay/Main.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace VotingDay
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VoteCounts = new System.Windows.Forms.DataGridView();
+            this.ItemNumberInputLabel = new System.Windows.Forms.Label();
+            this.ItemCount = new System.Windows.Forms.TextBox();
+            this.TeamCountLabel = new System.Windows.Forms.Label();
+            this.TeamCount = new System.Windows.Forms.TextBox();
+            this.SetupPlurality = new System.Windows.Forms.Button();
+            this.SetupCumulative = new System.Windows.Forms.Button();
+            this.SetupApproval = new System.Windows.Forms.Button();
+            this.SetupBorda = new System.Windows.Forms.Button();
+            this.SetupPluralityElimination = new System.Windows.Forms.Button();
+            this.SetupPairwise = new System.Windows.Forms.Button();
+            this.CustomLabel = new System.Windows.Forms.Label();
+            this.CustomInput = new System.Windows.Forms.TextBox();
+            this.AnalyzeButton = new System.Windows.Forms.Button();
+            this.CustomButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.VoteCounts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // VoteCounts
+            // 
+            this.VoteCounts.AllowUserToAddRows = false;
+            this.VoteCounts.AllowUserToDeleteRows = false;
+            this.VoteCounts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.VoteCounts.Location = new System.Drawing.Point(12, 73);
+            this.VoteCounts.Name = "VoteCounts";
+            this.VoteCounts.RowHeadersWidth = 10;
+            this.VoteCounts.Size = new System.Drawing.Size(361, 209);
+            this.VoteCounts.TabIndex = 0;
+            this.VoteCounts.Tag = "VoteCounts";
+            // 
+            // ItemNumberInputLabel
+            // 
+            this.ItemNumberInputLabel.AutoSize = true;
+            this.ItemNumberInputLabel.Location = new System.Drawing.Point(9, 9);
+            this.ItemNumberInputLabel.Name = "ItemNumberInputLabel";
+            this.ItemNumberInputLabel.Size = new System.Drawing.Size(70, 13);
+            this.ItemNumberInputLabel.TabIndex = 1;
+            this.ItemNumberInputLabel.Text = "Item Number:";
+            // 
+            // ItemCount
+            // 
+            this.ItemCount.Enabled = false;
+            this.ItemCount.Location = new System.Drawing.Point(98, 6);
+            this.ItemCount.Name = "ItemCount";
+            this.ItemCount.Size = new System.Drawing.Size(33, 20);
+            this.ItemCount.TabIndex = 2;
+            this.ItemCount.TextChanged += new System.EventHandler(this.ItemCount_TextChanged);
+            // 
+            // TeamCountLabel
+            // 
+            this.TeamCountLabel.AutoSize = true;
+            this.TeamCountLabel.Location = new System.Drawing.Point(9, 37);
+            this.TeamCountLabel.Name = "TeamCountLabel";
+            this.TeamCountLabel.Size = new System.Drawing.Size(77, 13);
+            this.TeamCountLabel.TabIndex = 3;
+            this.TeamCountLabel.Text = "Team Number:";
+            // 
+            // TeamCount
+            // 
+            this.TeamCount.Enabled = false;
+            this.TeamCount.Location = new System.Drawing.Point(98, 34);
+            this.TeamCount.Name = "TeamCount";
+            this.TeamCount.Size = new System.Drawing.Size(33, 20);
+            this.TeamCount.TabIndex = 4;
+            this.TeamCount.TextChanged += new System.EventHandler(this.TeamCount_TextChanged);
+            // 
+            // SetupPlurality
+            // 
+            this.SetupPlurality.Location = new System.Drawing.Point(393, 17);
+            this.SetupPlurality.Name = "SetupPlurality";
+            this.SetupPlurality.Size = new System.Drawing.Size(154, 33);
+            this.SetupPlurality.TabIndex = 5;
+            this.SetupPlurality.Text = "Set Up Plurality Vote";
+            this.SetupPlurality.UseVisualStyleBackColor = true;
+            this.SetupPlurality.Click += new System.EventHandler(this.SetupPlurality_Click);
+            // 
+            // SetupCumulative
+            // 
+            this.SetupCumulative.Location = new System.Drawing.Point(393, 56);
+            this.SetupCumulative.Name = "SetupCumulative";
+            this.SetupCumulative.Size = new System.Drawing.Size(154, 33);
+            this.SetupCumulative.TabIndex = 6;
+            this.SetupCumulative.Text = "Set Up Cumulative Vote";
+            this.SetupCumulative.UseVisualStyleBackColor = true;
+            this.SetupCumulative.Click += new System.EventHandler(this.SetupCumulative_Click);
+            // 
+            // SetupApproval
+            // 
+            this.SetupApproval.Location = new System.Drawing.Point(393, 95);
+            this.SetupApproval.Name = "SetupApproval";
+            this.SetupApproval.Size = new System.Drawing.Size(154, 33);
+            this.SetupApproval.TabIndex = 7;
+            this.SetupApproval.Text = "Set Up Approval Vote";
+            this.SetupApproval.UseVisualStyleBackColor = true;
+            this.SetupApproval.Click += new System.EventHandler(this.SetupApproval_Click);
+            // 
+            // SetupBorda
+            // 
+            this.SetupBorda.Location = new System.Drawing.Point(393, 134);
+            this.SetupBorda.Name = "SetupBorda";
+            this.SetupBorda.Size = new System.Drawing.Size(154, 33);
+            this.SetupBorda.TabIndex = 8;
+            this.SetupBorda.Text = "Set Up Borda Vote";
+            this.SetupBorda.UseVisualStyleBackColor = true;
+            this.SetupBorda.Click += new System.EventHandler(this.SetupBorda_Click);
+            // 
+            // SetupPluralityElimination
+            // 
+            this.SetupPluralityElimination.Location = new System.Drawing.Point(393, 173);
+            this.SetupPluralityElimination.Name = "SetupPluralityElimination";
+            this.SetupPluralityElimination.Size = new System.Drawing.Size(154, 33);
+            this.SetupPluralityElimination.TabIndex = 9;
+            this.SetupPluralityElimination.Text = "Set Up Plurality with Elim.";
+            this.SetupPluralityElimination.UseVisualStyleBackColor = true;
+            this.SetupPluralityElimination.Click += new System.EventHandler(this.SetupPluralityElimination_Click);
+            // 
+            // SetupPairwise
+            // 
+            this.SetupPairwise.Location = new System.Drawing.Point(393, 212);
+            this.SetupPairwise.Name = "SetupPairwise";
+            this.SetupPairwise.Size = new System.Drawing.Size(154, 33);
+            this.SetupPairwise.TabIndex = 10;
+            this.SetupPairwise.Text = "Set Up Pairwise Elimination";
+            this.SetupPairwise.UseVisualStyleBackColor = true;
+            this.SetupPairwise.Click += new System.EventHandler(this.SetupPairwise_Click);
+            // 
+            // CustomLabel
+            // 
+            this.CustomLabel.AutoSize = true;
+            this.CustomLabel.Location = new System.Drawing.Point(173, 12);
+            this.CustomLabel.Name = "CustomLabel";
+            this.CustomLabel.Size = new System.Drawing.Size(68, 13);
+            this.CustomLabel.TabIndex = 11;
+            this.CustomLabel.Text = "CustomLabel";
+            this.CustomLabel.Visible = false;
+            // 
+            // CustomInput
+            // 
+            this.CustomInput.Location = new System.Drawing.Point(272, 9);
+            this.CustomInput.Name = "CustomInput";
+            this.CustomInput.Size = new System.Drawing.Size(38, 20);
+            this.CustomInput.TabIndex = 12;
+            this.CustomInput.Visible = false;
+            // 
+            // AnalyzeButton
+            // 
+            this.AnalyzeButton.Location = new System.Drawing.Point(393, 279);
+            this.AnalyzeButton.Name = "AnalyzeButton";
+            this.AnalyzeButton.Size = new System.Drawing.Size(154, 46);
+            this.AnalyzeButton.TabIndex = 13;
+            this.AnalyzeButton.Text = "Analyze!";
+            this.AnalyzeButton.UseVisualStyleBackColor = true;
+            // 
+            // CustomButton
+            // 
+            this.CustomButton.Location = new System.Drawing.Point(194, 34);
+            this.CustomButton.Name = "CustomButton";
+            this.CustomButton.Size = new System.Drawing.Size(92, 23);
+            this.CustomButton.TabIndex = 14;
+            this.CustomButton.Text = "CustomButton";
+            this.CustomButton.UseVisualStyleBackColor = true;
+            this.CustomButton.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 341);
+            this.Controls.Add(this.CustomButton);
+            this.Controls.Add(this.AnalyzeButton);
+            this.Controls.Add(this.CustomInput);
+            this.Controls.Add(this.CustomLabel);
+            this.Controls.Add(this.SetupPairwise);
+            this.Controls.Add(this.SetupPluralityElimination);
+            this.Controls.Add(this.SetupBorda);
+            this.Controls.Add(this.SetupApproval);
+            this.Controls.Add(this.SetupCumulative);
+            this.Controls.Add(this.SetupPlurality);
+            this.Controls.Add(this.TeamCount);
+            this.Controls.Add(this.TeamCountLabel);
+            this.Controls.Add(this.ItemCount);
+            this.Controls.Add(this.ItemNumberInputLabel);
+            this.Controls.Add(this.VoteCounts);
+            this.Name = "Main";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.VoteCounts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView VoteCounts;
+        private System.Windows.Forms.Label ItemNumberInputLabel;
+        private System.Windows.Forms.TextBox ItemCount;
+        private System.Windows.Forms.Label TeamCountLabel;
+        private System.Windows.Forms.TextBox TeamCount;
+        private System.Windows.Forms.Button SetupPlurality;
+        private System.Windows.Forms.Button SetupCumulative;
+        private System.Windows.Forms.Button SetupApproval;
+        private System.Windows.Forms.Button SetupBorda;
+        private System.Windows.Forms.Button SetupPluralityElimination;
+        private System.Windows.Forms.Button SetupPairwise;
+        private System.Windows.Forms.Label CustomLabel;
+        private System.Windows.Forms.TextBox CustomInput;
+        private System.Windows.Forms.Button AnalyzeButton;
+        private System.Windows.Forms.Button CustomButton;
+    }
+}
+
---
+++ VotingDay/VotingDay/Main.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Configuration;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VotingDay
+{
+    public partial class Main : Form
+    {
+        private DataTable voteCounts = new DataTable();
+        private Mode mode;
+
+        public enum Mode
+        {
+            Plurality,
+            Cumulative,
+            Approval,
+            Borda,
+            PluralityElimination,
+            PairwiseElimination
+        }
+
+        public Main()
+        {
+            InitializeComponent();
+            VoteCounts.DataSource = voteCounts;
+        }
+
+        public void RestoreDefaults()
+        {
+            ItemCount.Enabled = false;
+            TeamCount.Enabled = false;
+            CustomLabel.Visible = false;
+            CustomInput.Visible = false;
+            CustomButton.Visible = false;
+            RemoveClickEvent(CustomButton);
+            RemoveClickEvent(AnalyzeButton);
+        }
+
+        private void ItemCount_TextChanged(object sender, EventArgs e)
+        {
+            int temp;
+            if (Int32.TryParse(ItemCount.Text, out temp))
+            {
+                if (voteCounts.Rows.Count < temp)
+                {
+                    for (var i = voteCounts.Rows.Count+1; i <= temp; i++)
+                    {
+                        voteCounts.Rows.Add(i, "", 0);
+                    }
+                }
+                else
+                {
+                    for (var i = voteCounts.Rows.Count-1; i >= temp; i--)
+                    {
+                        voteCounts.Rows.Remove(voteCounts.Rows[i]);
+                    }
+                }
+            }
+        }
+
+        private void TeamCount_TextChanged(object sender, EventArgs e)
+        {
+            int temp;
+            if (Int32.TryParse(TeamCount.Text, out temp))
+            {
+                if (voteCounts.Rows.Count < temp)
+                {
+                    for (var i = voteCounts.Rows.Count + 1; i <= temp; i++)
+                    {
+                        voteCounts.Rows.Add("Team " + i);
+                    }
+                }
+                else
+                {
+                    for (var i = voteCounts.Rows.Count - 1; i >= temp; i--)
+                    {
+                        voteCounts.Rows.Remove(voteCounts.Rows[i]);
+                    }
+                }
+            }
+        }
+
+        private void SetupPlurality_Click(object sender, EventArgs e)
+        {
+            mode = Mode.Plurality;
+            voteCounts = new DataTable();
+            voteCounts.Columns.Add("id", typeof(int));
+            voteCounts.Columns.Add("name", typeof(string));
+            voteCounts.Columns.Add("count", typeof(int));
+            VoteCounts.DataSource = voteCounts;
+            VoteCounts.AutoResizeColumns();
+            RestoreDefaults();
+            ItemCount.Enabled = true;
+        }
+
+        private void SetupCumulative_Click(object sender, EventArgs e)
+        {
+            mode = Mode.Cumulative;
+            voteCounts = new DataTable();
+            voteCounts.Columns.Add("id", typeof(int));
+            voteCounts.Columns.Add("name", typeof(string));
+            voteCounts.Columns.Add("count", typeof(int));
+            VoteCounts.DataSource = voteCounts;
+            VoteCounts.AutoResizeColumns();
+            RestoreDefaults();
+            CustomLabel.Text = "Vote Number(k)";
+            ItemCount.Enabled = true;
+            CustomLabel.Visible = true;
+            CustomInput.Visible = true;
+        }
+
+        private void SetupApproval_Click(object sender, EventArgs e)
+        {
+            mode = Mode.Approval;
+            voteCounts = new DataTable();
+            voteCounts.Columns.Add("id", typeof(int));
+            voteCounts.Columns.Add("name", typeof(string));
+            voteCounts.Columns.Add("count", typeof(int));
+            VoteCounts.DataSource = voteCounts;
+            VoteCounts.AutoResizeColumns();
+            RestoreDefaults();
+            ItemCount.Enabled = true;
+        }
+
+        private void SetupBorda_Click(object sender, EventArgs e)
+        {
+            mode = Mode.Borda;
+            voteCounts = new DataTable();
+            int temp;
+            if (Int32.TryParse(ItemCount.Text, out temp))
+            {
+                RestoreDefaults();
+                TeamCount.Enabled = true;
+                ItemCount.Enabled = false;
+                voteCounts.Columns.Add("Team Name");
+                for (var i = 1; i <= temp; i++)
+                {
+                    voteCounts.Columns.Add(i.ToString(), typeof (int));
+                }
+                
+                VoteCounts.DataSource = voteCounts;
+                VoteCounts.AutoResizeColumns();
+            }
+            else
+            {
+                //TODO display error of some sort ("ItemCount must be an int before Borda Voting").
+            }
+        }
+
+        private void SetupPluralityElimination_Click(object sender, EventArgs e)
+        {
+            mode = Mode.PluralityElimination;
+            voteCounts = new DataTable();
+            int temp;
+            if (Int32.TryParse(ItemCount.Text, out temp))
+            {
+                RestoreDefaults();
+                ItemCount.Enabled = false;
+                voteCounts.Columns.Add("Item Number");
+                for (var i = 1; i <= temp; i++)
+                {
+                    voteCounts.Columns.Add("Round " + i.ToString(), typeof(int));
+                }
+
+                for (var i = 1; i <= temp; i++)
+                {
+                    voteCounts.Rows.Add(i.ToString());
+                }
+
+                CustomLabel.Text = "Round:";
+                CustomInput.Text = 1.ToString();
+                CustomButton.Text = "Advance Round";
+                CustomLabel.Visible = true;
+                CustomInput.Visible = true;
+                CustomButton.Visible = true;
+                CustomButton.Click += AdvanceRound;
+
+                VoteCounts.DataSource = voteCounts;
+                VoteCounts.AutoResizeColumns();
+            }
+            else
+            {
+                //TODO display error of some sort ("ItemCount must be an int before Borda Voting").
+            }
+        }
+
+        private void AdvanceRound(object sender, EventArgs e)
+        {
+            int tempRound, tempItemCount;
+            if (Int32.TryParse(CustomInput.Text, out tempRound) && Int32.TryParse(ItemCount.Text, out tempItemCount))
+            {
+                //get row with minimum value
+                int minIndex = 0, minValue = Int32.MaxValue, temp1; 
+                for (var i = 0; i < tempItemCount; i++)
+                {
+                    if (Int32.TryParse(voteCounts.Rows[i][tempRound].ToString(), out temp1) && temp1 < minValue)
+                    {
+                        minIndex = i;
+                        minValue = temp1;
+                    }
+                }
+
+                for (var i = tempRound; i <= tempItemCount; i++)
+                {
+                    VoteCounts.Rows[minIndex].Cells[i].Style.BackColor = Color.Red;
+                    VoteCounts.Rows[minIndex].Cells[i].ReadOnly = true;
+                }
+
+                CustomInput.Text = (tempRound + 1).ToString();
+            }
+            else
+            {
+                //TODO display error of some sort (Round number must be an int you dummy!).
+            }
+        }
+
+        private void SetupPairwise_Click(object sender, EventArgs e)
+        {
+            mode = Mode.PairwiseElimination;
+            //TODO figure out what goes here
+        }
+
+        private void RemoveClickEvent(Button b)
+        {
+            FieldInfo f1 = typeof(Control).GetField("EventClick",
+                BindingFlags.Static | BindingFlags.NonPublic);
+            object obj = f1.GetValue(b);
+            PropertyInfo pi = b.GetType().GetProperty("Events",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+            EventHandlerList list = (EventHandlerList)pi.GetValue(b, null);
+            list.RemoveHandler(obj, list[obj]);
+        }
+
+        //TODO write analysis handlers
+    }
+}
