eb06e97394af2c0cf90f034db5418466ff3afa89
Brad Steiner <bradasteiner@gmail.com>
2015-02-27 00:01:20 -0600

    Calculate table for plurality calculation. Does not display yet.

--- VotingDay/VotingDay/Plurality.cs
+++ VotingDay/VotingDay/Plurality.cs
@@ -14,8 +14,67 @@ namespace VotingDay
     {
         public Plurality(DataTable input)
         {
+
             InitializeComponent();
+
+            Dictionary<int, int> dictionary =
+	            new Dictionary<int, int>();
+
+	        
+
+
+            for (int i = 0; i <input.Rows.Count; i++){
+                dictionary.Add( Convert.ToInt32(input.Rows[i][0]), Convert.ToInt32(input.Rows[i][2]));
+            }
+
+            //sort dictionary
+            List<KeyValuePair<int, int>> myList = dictionary.ToList();
+
+            myList.Sort(
+                delegate(KeyValuePair<int, int> firstPair,
+                KeyValuePair<int, int> nextPair)
+                {
+                    return nextPair.Value.CompareTo(firstPair.Value);
+                }
+            );
+
+            IDictionary<int, int> sortedDictionary =
+                myList.ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            DataTable table = new DataTable("Order");
+            for (int i = 0; i < 2; i++)
+            {
+                table.Columns.Add();
+            }
+            foreach (int id in sortedDictionary.Keys)
+            {
+                DataRow newRow = table.Rows.Add();
+                object[] rowArray = new object[2];
+                rowArray[0] = id;
+                rowArray[1] = sortedDictionary[id];
+                newRow.ItemArray = rowArray;
+            }
+
+            int a = 0;
+
+
+     
         }
+
+        ////TODO fix all of this
+        //private DataTable displayPreferences;
+        //private void displayOrder(object sender, EventArgs e)
+        //{
+        //    displayPreferences = new DataTable();
+        //    displayPreferences.Columns.Add("id", typeof(int));
+        //    //voteCounts.Columns.Add("name", typeof(string));
+        //    displayPreferences.Columns.Add("count", typeof(int));
+        //    DisplayPreferences.DataSource = displayPreferences;
+        //    DisplayPreferences.AutoResizeColumns();
+        //    RestoreDefaults();
+        //    //ItemCount.Enabled = true;
+        //    //AnalyzeButton.Click += AnalyzePlurality;
+        //}
 
         private void DismissButton_Click(object sender, EventArgs e)
         {
