a134a407916d067c70157232abbdc3833a79ef38
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-03-03 01:36:48 -0600

    added ability to paste

---
+++ VotingDay/VotingDay/Paste.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VotingDay
+{
+    public static class Paste
+    {
+        // PasteInData pastes clipboard data into the grid passed to it.
+        public static void PasteInData(ref DataGridView dgv)
+        {
+            char[] rowSplitter = { '\n', '\r' };  // Cr and Lf.
+            char columnSplitter = '\t';         // Tab.
+
+            IDataObject dataInClipboard = Clipboard.GetDataObject();
+
+            string stringInClipboard =
+                dataInClipboard.GetData(DataFormats.Text).ToString();
+
+            string[] rowsInClipboard = stringInClipboard.Split(rowSplitter,
+                StringSplitOptions.RemoveEmptyEntries);
+
+            int r = dgv.SelectedCells[0].RowIndex;
+            int c = dgv.SelectedCells[0].ColumnIndex;
+
+            if (dgv.Rows.Count < (r + rowsInClipboard.Length))
+                dgv.Rows.Add(r + rowsInClipboard.Length - dgv.Rows.Count);
+
+            // Loop through lines:
+
+            int iRow = 0;
+            while (iRow < rowsInClipboard.Length)
+            {
+                // Split up rows to get individual cells:
+
+                string[] valuesInRow =
+                    rowsInClipboard[iRow].Split(columnSplitter);
+
+                // Cycle through cells.
+                // Assign cell value only if within columns of grid:
+
+                int jCol = 0;
+                while (jCol < valuesInRow.Length)
+                {
+                    if ((dgv.ColumnCount - 1) >= (c + jCol))
+                        dgv.Rows[r + iRow].Cells[c + jCol].Value =
+                        valuesInRow[jCol];
+
+                    jCol += 1;
+                } // end while
+
+                iRow += 1;
+            } // end while
+        } // PasteInData
+    }
+}
