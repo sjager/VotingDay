25b7008f04084224dab01cc8eea509863aa5e5d0
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-03-02 19:46:22 -0600

    cumulative analysis done

--- VotingDay/VotingDay/Cumulative.cs
+++ VotingDay/VotingDay/Cumulative.cs
@@ -15,6 +15,45 @@ namespace VotingDay
         public Cumulative(DataTable input)
         {
             InitializeComponent();
+
+            var candidateList = new List<Candidate>();
+
+            foreach(DataRow dr in input.Rows)
+            {
+                Candidate rowCandidate = new Candidate()
+                {
+                    Name = dr[1].ToString(),
+                    Votes = Int16.Parse(dr[2].ToString())
+                };
+
+                candidateList.Add(rowCandidate);
+            }
+
+            var results = new VotingResults(candidateList);
+
+            cumulativeAnalysisDataGrid.ReadOnly = true;
+
+            var nameCol = new DataGridViewTextBoxColumn();
+            var votesCol = new DataGridViewTextBoxColumn();
+            var rankCol = new DataGridViewTextBoxColumn();
+
+            nameCol.HeaderText = "Name";
+            votesCol.HeaderText = "Votes";
+            rankCol.HeaderText = "Rank";
+
+            cumulativeAnalysisDataGrid.Columns.AddRange(new DataGridViewColumn[] { rankCol, nameCol, votesCol });
+
+            int rank = 1;
+            foreach(Candidate c in results.Candidates)
+            {
+                AddRow(c, rank);
+                rank++;
+            }
+        }
+
+        public void AddRow(Candidate candidate, int rank)
+        {
+            cumulativeAnalysisDataGrid.Rows.Add(rank, candidate.Name, candidate.Votes);
         }
 
         private void DismissButton_Click(object sender, EventArgs e)
