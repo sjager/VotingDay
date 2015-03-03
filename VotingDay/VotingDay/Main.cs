a8d52302952a18e20e8aead2f1ae583010912482
Sawyer Jager <sawyer.jgr@gmail.com>
2015-03-02 18:28:53 -0600

    Update Plurality/Elimination interface.

--- VotingDay/VotingDay/Main.cs
+++ VotingDay/VotingDay/Main.cs
@@ -212,7 +212,7 @@ namespace VotingDay
                 int minIndex = 0, minValue = Int32.MaxValue, temp1; 
                 for (var i = 0; i < tempItemCount; i++)
                 {
-                    if (Int32.TryParse(voteCounts.Rows[i][tempRound].ToString(), out temp1) && temp1 < minValue)
+                    if (VoteCounts.Rows[i].Cells[tempRound].Style.BackColor != Color.Red && Int32.TryParse(voteCounts.Rows[i][tempRound].ToString(), out temp1) && temp1 < minValue)
                     {
                         minIndex = i;
                         minValue = temp1;
@@ -221,10 +221,17 @@ namespace VotingDay
 
                 for (var i = tempRound; i <= tempItemCount; i++)
                 {
-                    VoteCounts.Rows[minIndex].Cells[i].Style.BackColor = Color.Red;
-                    VoteCounts.Rows[minIndex].Cells[i].ReadOnly = true;
+                    for (var j = 0; j < VoteCounts.RowCount; j++)
+                    {
+                        if (j == minIndex)
+                        {
+                            VoteCounts.Rows[j].Cells[i].Style.BackColor = Color.Red;
+                            VoteCounts.Rows[minIndex].Cells[i].ReadOnly = true;
+                        }
+                        VoteCounts.Rows[j].Cells[i].Value = Convert.ToInt32(voteCounts.Rows[j][tempRound]);
+                    }
                 }
-                CustomInput.Text = (tempRound + 1).ToString();
+                CustomInput.Text = (++tempRound).ToString();
             }
             else
             {
